e29d031c5291f7c722dbe4f00e0eb90117f106b1
Amar-lenovo <amarvadla27@gmail.com>
2020-03-08 15:45:46 +0530

    added clear button

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const UserItem = props => {
   const { avatar_url, login, html_url } = props.user;
 
   return (
     <div className='card text-center'>
-      <img
+      <img alt={avatar_url}
         src={avatar_url}
         className='round-img'
         style={{ width: '60px' }}
