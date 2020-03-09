d249640ceccd2d361b492a43a6282bb95061238a
Amar-lenovo <amarvadla27@gmail.com>
2020-03-09 14:06:04 +0530

    added search alert

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
-import './App.css';
-import axios from 'axios';
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   // async componentDidMount() {
@@ -20,7 +22,7 @@ class App extends Component {
   //   this.setState({ users: res.data, loading: false });
   // }
 
-  searchUsers = async (text) => {
+  searchUsers = async text => {
     console.log(text);
     this.setState({ loading: true });
 
@@ -28,21 +30,33 @@ class App extends Component {
       `https:api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
     this.setState({ users: res.data.items, loading: false });
-  }
+  };
 
   clearUsers = () => {
     this.setState({
       users: [],
       loading: false
-    })
-  }
+    });
+  };
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type } });
+
+    setTimeout(() => this.setState({ alert: null }), 2000);
+  };
 
   render() {
     return (
-      <div className='app'>
-        <Navbar title='Github Finder' />
-        <div className='container'>
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length > 0 ? true : false} />
+      <div className="app">
+        <Navbar title="Github Finder" />
+        <div className="container">
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,37 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Search extends Component {
+  state = {
+    text: ""
+  };
 
-    state = {
-        text: ''
-    }
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-    onChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
+  onSubmit = e => {
+    e.preventDefault();
+    if (this.state.text === "") {
+      this.props.setAlert("Please enter something", "light");
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: "" });
     }
+  };
 
-    onSubmit = (e) => {
-        e.preventDefault();
-        this.props.searchUsers(this.state.text)
-        this.setState({ text: '' })
-    }
-
-    render() {
-        return (
-            <div>
-                <form className='form' onSubmit={this.onSubmit.bind(this)}>
-                    <input type="text" name="text" placeholder="Search users..." value={this.state.text}
-                        onChange={this.onChange}></input>
-                    <input type="submit" value="Search" className="btn btn-dark btn-block" />
-                </form>
-                {this.props.showClear &&
-                 <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>
-                 }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <form className="form" onSubmit={this.onSubmit.bind(this)}>
+          <input
+            type="text"
+            name="text"
+            placeholder="Search users..."
+            value={this.state.text}
+            onChange={this.onChange}
+          ></input>
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-dark btn-block"
+          />
+        </form>
+        {this.props.showClear && (
+          <button
+            className="btn btn-light btn-block"
+            onClick={this.props.clearUsers}
+          >
+            Clear
+          </button>
+        )}
+      </div>
+    );
+  }
 }
 
-export default Search
+export default Search;
